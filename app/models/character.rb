156c41adbd24bdc031d6663d90bbf247099633eb
Chong Kim <ckim80@chicagobooth.edu>
2017-05-18 14:12:52 -0500

    Revert "First Commit"
    This reverts commit b8ad9ba7e17f66fc108c291a792794c3b939b7da.

--- app/models/character.rb
+++
@@ -1,9 +0,0 @@
-class Character < ApplicationRecord
-  belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
- #  Character:
- # - movie_id: must be present
-  validates :movie_id, :presence => true
- # - actor_id: must be present
-  validates :actor_id, :presence => true
- # - name: no rules
-end
