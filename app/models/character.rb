b8ad9ba7e17f66fc108c291a792794c3b939b7da
Chong Kim <ckim80@chicagobooth.edu>
2017-05-18 12:52:18 -0500

    First Commit

---
+++ app/models/character.rb
@@ -0,0 +1,9 @@
+class Character < ApplicationRecord
+  belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
+ #  Character:
+ # - movie_id: must be present
+  validates :movie_id, :presence => true
+ # - actor_id: must be present
+  validates :actor_id, :presence => true
+ # - name: no rules
+end
